c4497dcf12415cb2262143b70b018b3e3c2db3dc
Anton Serdyuchenko <anton415@gmail.com>
2024-12-09 11:10:28 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Anton Serdyuchenko on 09.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
